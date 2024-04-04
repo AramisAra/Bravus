032ec3433cf1d218c677e16cd0016375e96ec308
AramisAra <martinezcruzaj@gmail.com>
2024-04-04 16:23:23 -0400

    Update Grooming_Scheduler version to 1.0.0+4d822878ba863cab4861ad8ee05d5d853386b0c7

--- Grooming_Scheduler/obj/Debug/net8.0/Grooming_Scheduler.AssemblyInfo.cs
+++ Grooming_Scheduler/obj/Debug/net8.0/Grooming_Scheduler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grooming_Scheduler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23a0995615712b7955beee8ea6f2b3d7033e5a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d822878ba863cab4861ad8ee05d5d853386b0c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grooming_Scheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grooming_Scheduler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
