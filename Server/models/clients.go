518c65a301a1b0cdb7a6d478e4a57acff0817d6c
AramisAra <martinezcruzaj@gmail.com>
2024-06-16 11:05:32 -0400

    adding the testing changes and the start of login system

--- Server/models/clients.go
+++ Server/models/clients.go
@@ -5,6 +5,7 @@ type Client struct {
 	Base
 	Full_Name    string        `json:"full_name"`
 	Email        string        `json:"email"`
+	Password     string        `json:"password"`
 	Phone        uint          `json:"phone"`
 	Appointments []Appointment `gorm:"foreignKey:ClientID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"appointments"`
 	Animals      []Animals     `gorm:"foreignKey:Client_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"animals"`
