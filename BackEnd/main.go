7784e855f965c8b84f1f4c83e0ec6ea03955d9de
AramisAra <martinezcruzaj@gmail.com>
2024-06-18 21:10:13 -0400

    I have Remade main.go for it to divide the routes in to group routers. Have added the Client router, Animal router, and Appointment router

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -5,12 +5,41 @@ import (
 	"os"
 
 	database "github.com/AramisAra/BravusBackend/database"
+	"github.com/AramisAra/BravusBackend/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
 func HealthCheck(c *fiber.Ctx) error {
 	return c.SendString("OK")
+}
+
+func loginSystem(app *fiber.App) {
+	// login system
+	login := app.Group("/login")
+	login.Post("/Rowner", handlers.RegisterOwner)
+	login.Post("/Rclient", handlers.RegisterClient)
+	login.Post("/Lowner", handlers.LoginOwner)
+	login.Post("/Lclient", handlers.LoginClient)
+}
+func DatabaseHandlers(app *fiber.App) {
+	// Client Routes
+	client := app.Group("/client")
+	client.Get("/get/:uuid", handlers.GetClient)
+	client.Get("/get", handlers.ListClients)
+	client.Put("/update/:uuid", handlers.UpdateClient)
+	client.Delete("/delete/:uuid", handlers.DeleteClient)
+	// Animal Routes
+	animal := app.Group("/animal")
+	animal.Post("/create/:uuid", handlers.CreateAnimal)
+	animal.Delete("/delete/:uuid", handlers.DeleteAnimal)
+	animal.Put("/update/:uuid", handlers.UpdateAnimal)
+	// Appointment Routes
+	appointment := app.Group("/appointment")
+	appointment.Post("/create/:uuid/:uuid/uuid", handlers.CreateAppointment)
+	appointment.Get("/get/:uuid", handlers.GetAppointment)
+	appointment.Delete("/delete/:uuid", handlers.DeleteAppointment)
+	appointment.Put("/update/:uuid", handlers.UpdateAppointment)
 }
 
 func main() {
@@ -24,6 +53,8 @@ func main() {
 
 	// This is the main overall the app_api
 	app := fiber.New()
+	loginSystem(app)
+	DatabaseHandlers(app)
 
 	app.Listen(":8000")
 }
