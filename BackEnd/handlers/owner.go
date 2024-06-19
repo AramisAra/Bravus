1695fd297d3c4c861e48a48f217a03b89ef7493f
AramisAra <martinezcruzaj@gmail.com>
2024-06-19 11:52:06 -0400

    I have add the doc to this file for better understanding

--- BackEnd/handlers/owner.go
+++ BackEnd/handlers/owner.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
+	"time"
+
+	"github.com/AramisAra/BravusBackend/config"
 	database "github.com/AramisAra/BravusBackend/database"
 	"github.com/AramisAra/BravusBackend/database/dbmodels"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Register Owner func
 func RegisterOwner(c *fiber.Ctx) error {
 	registration := new(dbmodels.RegisterRequestOwner)
 	if err := c.BodyParser(registration); err != nil {
@@ -43,6 +48,42 @@ func RegisterOwner(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusCreated).JSON(owner)
 }
 
+// Login Owner Func
+func LoginOwner(c *fiber.Ctx) error {
+	login := dbmodels.Login{}
+	if err := c.BodyParser(&login); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	// Checkes if the email is in the database
+	var owner dbmodels.Owner
+	result := database.Database.Db.Find(&owner, "Email = ?", login.Email)
+	if result.Error != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid email or password"})
+	}
+
+	// Checkes hash password
+	if err := bcrypt.CompareHashAndPassword([]byte(owner.Password), []byte(login.Password)); err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid Password"})
+	}
+
+	// Creates the Claims
+	claims := jwt.MapClaims{
+		"ID":    owner.ID,
+		"email": owner.Email,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(), // Token expiry set to 24 hours
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(config.Secret))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "could not login"})
+	}
+
+	// Return the token
+	return c.JSON(dbmodels.LoginResponse{Token: t})
+}
+
+// List all owners
 func ListOwners(c *fiber.Ctx) error {
 	owners := []dbmodels.Owner{}
 
