ace5b4d28ed34f35501b908313490231cd3a5567
AramisAra <martinezcruzaj@gmail.com>
2024-06-19 11:51:26 -0400

    I have transfer the login func to their corespening files and  left the protected route test in this file

--- BackEnd/handlers/login.go
+++ BackEnd/handlers/login.go
@@ -1,80 +1,11 @@
 package handlers
 
 import (
-	"time"
-
-	"github.com/AramisAra/BravusBackend/config"
-	database "github.com/AramisAra/BravusBackend/database"
-	dbmodels "github.com/AramisAra/BravusBackend/database/dbmodels"
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	jtoken "github.com/golang-jwt/jwt/v4"
-	"golang.org/x/crypto/bcrypt"
 )
 
-func LoginClient(c *fiber.Ctx) error {
-	login := dbmodels.Login{}
-	if err := c.BodyParser(&login); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	var client dbmodels.Client
-	result := database.Database.Db.Find(&client, "Email = ?", login.Email)
-	if result.Error != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid email or password"})
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(client.Password), []byte(login.Password)); err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid Password"})
-	}
-
-	claims := jwt.MapClaims{
-		"ID":    client.ID,
-		"email": client.Email,
-		"exp":   time.Now().Add(time.Hour * 24).Unix(), // Token expiry set to 24 hours
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	t, err := token.SignedString([]byte(config.Secret))
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "could not login"})
-	}
-
-	// Return the token
-	return c.JSON(dbmodels.LoginResponse{Token: t})
-
-}
-func LoginOwner(c *fiber.Ctx) error {
-	login := dbmodels.Login{}
-	if err := c.BodyParser(&login); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	var owner dbmodels.Owner
-	result := database.Database.Db.Find(&owner, "Email = ?", login.Email)
-	if result.Error != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid email or password"})
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(owner.Password), []byte(login.Password)); err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid Password"})
-	}
-
-	claims := jwt.MapClaims{
-		"ID":    owner.ID,
-		"email": owner.Email,
-		"exp":   time.Now().Add(time.Hour * 24).Unix(), // Token expiry set to 24 hours
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	t, err := token.SignedString([]byte(config.Secret))
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "could not login"})
-	}
-
-	// Return the token
-	return c.JSON(dbmodels.LoginResponse{Token: t})
-
-}
-
+// Login Test Route
 func Protected(c *fiber.Ctx) error {
 	user := c.Locals("user").(*jtoken.Token) // Ensure 'user' matches the ContextKey in middleware
 	claims := user.Claims.(jtoken.MapClaims)
