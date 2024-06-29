68544b2cec5ea1efb67dc631cb9d270aa93a86fd
yene2024 <yeneishla1524@gmail.com>
2024-06-29 12:55:54 -0400

    node modules

---
+++ frontend/build/precache-manifest.7d5bab3535808592b8a334fa2541710e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef39f66ac5bfeab7f8171a03367203a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e7e03c3f95bfc957fda",
+    "url": "/static/css/main.e09ce447.chunk.css"
+  },
+  {
+    "revision": "9c601455cb2301fe365c",
+    "url": "/static/js/2.53dfb7cb.chunk.js"
+  },
+  {
+    "revision": "0e7e03c3f95bfc957fda",
+    "url": "/static/js/main.8cfb78c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
