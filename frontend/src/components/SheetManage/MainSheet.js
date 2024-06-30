66c25f3d247bead6aa9f25acd3c2f0300ed44bb1
AramisAra <martinezcruzaj@gmail.com>
2024-06-29 22:14:46 -0400

    adding extra code of main sheet

--- frontend/src/components/SheetManage/MainSheet.js
+++ frontend/src/components/SheetManage/MainSheet.js
@@ -18,4 +18,12 @@ function MainSheet(){
             }
         }
     })
+    const Sheet = () => {
+        return (
+          <div className="min-h-screen bg-gray-900 text-white p-4 flex-grow">
+            <h1 className="text-2xl font-bold">Sheet</h1>
+            <p className="mt-4">This is the Sheet page.</p>
+          </div>
+        );
+      };
 }
