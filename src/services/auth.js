97667248a460f713586d14d51201ad0832955793
AramisAra <martinezcruzaj@gmail.com>
2024-07-11 14:52:55 -0400

    Testing auth changes

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 export const isAuthenticated = () => {
     return !!localStorage.getItem('token');
   };
   
 export const Auth = () => {
+    const navigate = useNavigate();
     useEffect(() => {
         // Replace with your backend URL
         const uuid = localStorage.getItem('uuid');
         // Generate this on the backend or frontend
         axios.get(`https://br-avus.com:8000/sheetapi/auth?uuid=${uuid}`)
             .then(response => {
-                window.location.href = response.data.url;
+                navigate(response.data.url)
             })
             .catch(error => {
                 console.error("Error during authentication", error);
