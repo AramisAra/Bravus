2e0f225626cc15c86a16f2a09f15d6a644757866
AramisAra <martinezcruzaj@gmail.com>
2024-07-11 15:29:23 -0400

    testing

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -83,7 +83,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://172.24.195.132:3000, https://aramisara.github.io, http://34.204.43.154:3000",
+		AllowOrigins:     "*",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
