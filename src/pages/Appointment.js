1d25fdb6ee77cd3ce47daf340ad2063eb40558f8
AramisAra <martinezcruzaj@gmail.com>
2024-07-11 03:10:33 -0400

    Reverting some changes

--- src/pages/Appointment.js
+++ src/pages/Appointment.js
@@ -25,7 +25,7 @@ function Appointment() {
   useEffect(() => {
     const fetchOwners = async () => {
         try {
-            const response = await fetch('https://3.89.162.4:8000/owner/get');  // Ensure this is a function call
+            const response = await fetch('http://3.89.162.4:8000/owner/get');  // Ensure this is a function call
             const data = await response.json();
             setOwners(data);
             setServices(data.services)
