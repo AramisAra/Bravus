6d04062762d4f422dea5abad1f9b9d203353a946
AramisAra <martinezcruzaj@gmail.com>
2024-07-24 13:31:32 -0400

    Making branches changes

--- BravusApp/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
-Vue.config.devtools = true;
--- BravusServer/database/models/animal.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Animal struct {
-	Base
-	ID      string `json:"id,omitempty"`
-	Name    string `json:"name"`
-	Species string `json:"species"`
-	OwnerID string `json:"owner_id"`
-}
