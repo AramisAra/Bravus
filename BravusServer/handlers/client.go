4912907596c0dc88b0fc83e069a239d8f17c878f
AramisAra <martinezcruzaj@gmail.com>
2024-08-05 01:49:06 -0400

    Last Time Editing Client handler fix all the variable names.  Adding register edits to make it vheck if the Email that is being given is in use.

--- BravusServer/handlers/client.go
+++ BravusServer/handlers/client.go
@@ -12,11 +12,7 @@ import (
 	"github.com/surrealdb/surrealdb.go"
 )
 
-// CreateClient creates a new client in the database.
-// It takes a fiber.Ctx object as a parameter and returns an error.
-// The function parses the request body into a models.Client object,
-// creates a new client in the database using the connectDb function,
-// and returns the created client as a JSON response.
+// This function is for making
 func RegisterClient(c *fiber.Ctx) error {
 
 	db := database.ConnectDb()
@@ -29,31 +25,52 @@ func RegisterClient(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	hashedPassword, err := utils.HashPassword(client.Password)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Error": "Failed to crypt"})
-	}
-	client.Password = hashedPassword
-
-	// Construct and execute the query with placeholders and a map for the values
-	query := "CREATE Client CONTENT {name: $name, email: $email, password: $password, phone: $phone, createdAt: $createdAt, updateAt: $updateAt, animals: $animals, appointments: $appointments}"
-	values := map[string]interface{}{
-		"createdAt":    createdAt,
-		"updateAt":     createdAt,
-		"name":         client.Name,
-		"phone":        client.Phone,
-		"email":        client.Email,
-		"password":     client.Password,
-		"animals":      client.Animals,
-		"appointments": client.Appointments,
+	// Check if the email is already in use
+	emailCheckQuery := `SELECT * FROM Client, Owner WHERE email = $email`
+	emailCheckValues := map[string]interface{}{
+		"email": client.Email,
 	}
 
-	_, err = db.Query(query, values)
+	emailCheckResponse, err := db.Query(emailCheckQuery, emailCheckValues)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	return c.Status(200).JSON(client)
+	// Assuming response is a JSON array, unmarshal into a slice of maps
+	var emailCheckResult []map[string]interface{}
+	if err := surrealdb.Unmarshal(emailCheckResponse, &emailCheckResult); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	resultData, ok := emailCheckResult[0]["result"].([]interface{})
+	if !ok || len(resultData) == 0 {
+		hashedPassword, err := utils.HashPassword(client.Password)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Error": "Failed to crypt"})
+		}
+		client.Password = hashedPassword
+
+		// Construct and execute the query with placeholders and a map for the values
+		query := "CREATE Client CONTENT {name: $name, email: $email, password: $password, phone: $phone, createdAt: $createdAt, updateAt: $updateAt, animals: $animals, appointments: $appointments}"
+		values := map[string]interface{}{
+			"createdAt":    createdAt,
+			"updateAt":     createdAt,
+			"name":         client.Name,
+			"phone":        client.Phone,
+			"email":        client.Email,
+			"password":     client.Password,
+			"animals":      client.Animals,
+			"appointments": client.Appointments,
+		}
+
+		_, err = db.Query(query, values)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		return c.Status(200).JSON(client)
+	}
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "email already exist"})
 }
 
 // LoginClient handles the login request for a client.
@@ -63,11 +80,6 @@ func RegisterClient(c *fiber.Ctx) error {
 //	  "email": "user@example.com",
 //	  "password": "password123"
 //	}
-//
-// It retrieves the client from the database based on the provided email,
-// verifies the password, and generates a JWT token if the login is successful.
-// The token is returned in the response along with the client ID.
-// If the email or password is invalid, it returns an error response.
 func LoginClient(c *fiber.Ctx) error {
 	var input struct {
 		Email    string `json:"email"`
@@ -83,11 +95,11 @@ func LoginClient(c *fiber.Ctx) error {
 
 	// Query for the client by email
 	query := `SELECT * FROM Client WHERE email = $email`
-	params := map[string]interface{}{
+	values := map[string]interface{}{
 		"email": input.Email,
 	}
 
-	response, err := db.Query(query, params)
+	response, err := db.Query(query, values)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
@@ -96,9 +108,6 @@ func LoginClient(c *fiber.Ctx) error {
 	var results []map[string]interface{}
 	if err := surrealdb.Unmarshal(response, &results); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-	}
-	if len(results) == 0 {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid email or password"})
 	}
 
 	// Extract the password from the nested structure
@@ -139,8 +148,6 @@ func LoginClient(c *fiber.Ctx) error {
 }
 
 // ListClient handles the request to list all clients.
-// It connects to the database, queries the "Client" table, and returns the response as JSON.
-// ListClient handles the request to list all clients.
 func ListClient(c *fiber.Ctx) error {
 	db := database.ConnectDb()
 	defer db.Close()
@@ -180,15 +187,7 @@ func GetClient(c *fiber.Ctx) error {
 	return c.Status(200).JSON(response)
 }
 
-// UpdateClient updates the client information in the database based on the provided ID.
-// It expects a query parameter 'id' to be passed in the request URL.
-// If the 'id' is empty or not a valid ID, it returns a JSON response with an error message.
-// It fetches the existing client data from the database using the provided ID.
-// If the client is not found, it returns a JSON response with an error message.
-// It parses the request body into a client struct and updates the corresponding fields in the existing client data.
-// It updates the 'updateAt' field with the current timestamp.
-// It builds an update query and executes it to update the client information in the database.
-// Finally, it returns a JSON response with the updated client data.
+// UpdateClient updates the client information in the database based on the provided ID..
 func UpdateClient(c *fiber.Ctx) error {
 	id := c.Query("id")
 	if id == "" {
@@ -205,11 +204,11 @@ func UpdateClient(c *fiber.Ctx) error {
 
 	// Fetch the existing client data from the database
 	query := `SELECT * FROM Client WHERE id = $id`
-	params := map[string]interface{}{
+	values := map[string]interface{}{
 		"id": id,
 	}
 
-	response, err := db.Query(query, params)
+	response, err := db.Query(query, values)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
@@ -261,7 +260,7 @@ func UpdateClient(c *fiber.Ctx) error {
 
 	// Build the update query
 	updateQuery := "UPDATE $id CONTENT {name: $name, email: $email, phone: $phone, password: $password, createdAt: $createdAt, updateAt: $updateAt, animals: $animals, appointments: $appointments}"
-	values := map[string]interface{}{
+	updateValues := map[string]interface{}{
 		"id":           id,
 		"createdAt":    existingClientData["createdAt"],
 		"updateAt":     updateAt,
@@ -274,7 +273,7 @@ func UpdateClient(c *fiber.Ctx) error {
 	}
 
 	// Execute the update query
-	if _, err := db.Query(updateQuery, values); err != nil {
+	if _, err := db.Query(updateQuery, updateValues); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
