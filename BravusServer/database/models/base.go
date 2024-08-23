5a330451883ab3090e910c5770cbacfafdef7b05
AramisAra <martinezcruzaj@gmail.com>
2024-08-23 16:07:42 -0400

    Adding docs

--- BravusServer/database/models/base.go
+++ BravusServer/database/models/base.go
@@ -2,6 +2,7 @@ package models
 
 import "time"
 
+// This struct hold the base info on all the tables
 type Base struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
--- BravusServer/database/models/client.go
+++ BravusServer/database/models/client.go
@@ -1,5 +1,15 @@
 package models
 
+// Struct that holds the information of the Database
+// This is an example of the structs info
+//
+//	{
+//		"ID": "Client:304fks31sax"
+//		"Name": "John Doe"
+//		"Phone": "123-243-5677"
+//		"Email": "nn@nn.com"
+//		"Password": "HashPassword this will not show up in plaintext"
+//	}
 type Client struct {
 	Base
 	ID           string        `json:"id,omitempty"`
