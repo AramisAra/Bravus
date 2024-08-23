ee697de4a99f50d66c7e5f8d2384388bdafd68df
AramisAra <martinezcruzaj@gmail.com>
2024-08-23 13:21:20 -0400

    Created The OPattern config variable and added doc for each variable

--- BravusServer/config/config.go
+++ BravusServer/config/config.go
@@ -4,4 +4,11 @@ import (
 	"os"
 )
 
+// This contains the ClientIDPattern
+var CPattern = os.Getenv("CIDPattern")
+
+// This contains the OwnerIDPattern
+var OPattern = os.Getenv("OIDPattern")
+
+// This contains JWT_Secret use to sign
 var Secret = os.Getenv("JWT_SECRET")
