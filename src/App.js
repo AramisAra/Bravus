1a4f1387741d1726343c01ac35d3e0a0aaef8eba
AramisAra <martinezcruzaj@gmail.com>
2024-10-16 22:30:08 -0400

    Feat: Adding the current signUp page to start editing and fixing the process of login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import AppRouter from './router/AppRouter';
-import './styles/index.css';
+import React from "react";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'gray-900': '#1a1a1a',
-        'gray-800': '#2a2a2a',
-        'gray-700': '#3a3a3a',
-        'gray-600': '#4a4a4a',
-        'blue-500': '#3b82f6',
-        'blue-400': '#60a5fa',
-        'blue-600': '#2563eb',
-        'indigo-600': '#4f46e5',
+        "gray-900": "#1a1a1a",
+        "gray-800": "#2a2a2a",
+        "gray-700": "#3a3a3a",
+        "gray-600": "#4a4a4a",
+        "blue-500": "#3b82f6",
+        "blue-400": "#60a5fa",
+        "blue-600": "#2563eb",
+        "indigo-600": "#4f46e5",
+        "Raisin-Black": "#2C2B3C",
+        Onyx: "#403F4C",
+        Gunmetal: "#1B2432",
+        "African-Violet": "#B07BAC",
       },
     },
   },
