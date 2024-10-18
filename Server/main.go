7ca2e515766ee5f702e7f0e0dabf5e2d611d7653
AramisAra <martinezcruzaj@gmail.com>
2024-10-18 09:17:05 -0400

    Transfer: adding the main

--- Server/main.go
+++ Server/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Env File is Missing")
+	}
 
+	ClerkSecret := os.Getenv("Clerk_Secret")
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
